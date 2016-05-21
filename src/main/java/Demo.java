6436eebc2610f2f11e4c34af327b268cfafa104e
Viktoria <gluchka@ukr.net>
2016-05-21 23:12:30 +0300

    minor changes

--- src/main/java/Demo.java
+++ src/main/java/Demo.java
@@ -1,5 +1,3 @@
-/**
- * Created by Виктория on 02.02.2016.
- */
+
 public class Demo {
 }
--- src/test/java/DemoTest.java
+++ src/test/java/DemoTest.java
@@ -1,3 +1,11 @@
+import org.testng.Assert;
+import org.testng.annotations.Test;
 
 public class DemoTest {
+
+
+    @Test
+    public void test() {
+        Assert.assertEquals("test", "test");
+    }
 }
