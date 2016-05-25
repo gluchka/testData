09a1535f48b9e075bcd49a56db746dc58b927830
Viktoria <gluchka@ukr.net>
2016-05-25 22:29:06 +0300

    minor changes

--- src/test/java/DemoTest.java
+++ src/test/java/DemoTest.java
@@ -1,11 +1,26 @@
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
 import org.testng.Assert;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
 public class DemoTest {
+    private WebDriver driver;
 
+    @BeforeMethod
+    public void before() {
+        driver = new FirefoxDriver();
+    }
 
     @Test
     public void test() {
-        Assert.assertEquals("test", "test");
+        driver.get("https://www.ukr.net/");
+        Assert.assertTrue(driver.getCurrentUrl().contains("ukr.net"));
+    }
+
+    @AfterMethod
+    public  void after(){
+        driver.quit();
     }
 }
